09060dcf6b7418248f662a4d38c4b556ceb12a91
Sylvain Le Grill <sylvain@Sylvains-MacBook-Pro.local>
2019-07-09 15:38:59 +0200

    add Spain Link

---
+++ main.js
@@ -0,0 +1,11 @@
+var buttons = document.getElementsByTagName('button');
+
+var button = buttons[0];
+
+var inputs = document.getElementsByTagName('input');
+
+var input = inputs[0];
+
+button.onclick = function(){
+    window.open('https://en.wikipedia.org/wiki/Spain');
+};
